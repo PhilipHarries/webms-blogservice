e9112f828a9fc509cb22488d6a290b8f1cd51d4a
PhilipHarries <philip.harries.unix.consulting@gmail.com>
2016-04-08 18:14:21 +0100

    Simplified stop.sh

--- stop.sh
+++ stop.sh
@@ -1,9 +1,5 @@
 [[ -f ../blogservice.pid ]] && kill -9 $( cat ../blogservice.pid )
-for X in $( sudo netstat -npa|grep 5481|grep LISTEN|awk '{print $NF}'|sed s%/python%% );do
-    for Y in $( ps -ef|grep python|grep blogservice|awk '{print $2}' );do
-        echo $Y
-        kill -9 $Y
-    done
-    echo $X
-    kill -9 $X
+for Y in $( ps -ef|grep python|grep blogservice|awk '{print $2}' );do
+    echo $Y
+    kill -9 $Y
 done
